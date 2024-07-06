b8c0034003bd8793b6d35669f898e55bc5fb47dd
Danceny Karla <dancenykarla@gmail.com>
2024-07-06 15:36:24 +0200

    v02

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ Gestiona la paginación para mostrar diferentes conjuntos de Pokémon. De 10 en 
 "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10"
 
 
-nombre= pokemons.results[i].name
-foto = 
-
+- navegar entre las páginas de Pokémon
+- buscar Pokémon específicos 
+- ver detalles básicos de cada uno.
 */
 
 const searchBtn = document.getElementById("searchBtn")
@@ -18,33 +18,58 @@ const prevBtn = document.getElementById("prevBtn")
 const nextBtn = document.getElementById("nextBtn")
 const resetBtn = document.getElementById("resetBtn")
 const containerApp = document.getElementById("app")
-let pokemonName; 
 
-const urlBase = "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10";
+
+const urlBase = "https://pokeapi.co/api/v2/pokemon";
+
+let offset = 10; 
 
 
 const getPokemons = async () => {
     try{
-        const response = await fetch(urlBase)
-        const data = await response.json();
-        const pokemonName = data.results[1].namename;
-        const pokemonImage = data.sprites.ofront_default;
-        const template = `
+        const response = await fetch(`${urlBase}?offset=${offset}&limit=10`)
+        const pokemons = await response.json();
+        console.log(pokemons)
+        const pokemonArray = pokemons.results
+        return pokemonArray
+
+        //console.log(data.results[2].url.other.dream_world.front_default)
+        //console.log(data.sprites.other.dream_world.front_default)
+        } catch (error) {
+        console.log("Error al obtener el pokemon", error)
+}};
+
+
+getPokemons (); 
+
+getPokemons().then(pokemonArray =>{
+    pokemonArray.forEach(pokemon => {
+       console.log(pokemon.name) 
+       
+       
+        let template = `<div class="cardPokemon">
+        <h2><span>Nombre:</span>${pokemon.name}</h2>
+        </div>` 
+        containerApp.innerHTML += template
+    });
+}); 
+
+nextBtn.addEventListener("click", ()=>{
+    offset +=10; 
+})
+
+
+
+/*
+
+
+       const template = `
             <div class="card">
                 <h2><span>Nombre:</span> ${pokemonName}</h2>
                 <img class="imagen" src="${pokemonImage}" alt="${pokemonName}">
             </div>`;
 
         containerApp.innerHTML = template;
-   
-        } catch (error) {
-        console.log("Error al obtener el pokemon", error)
-}}
-
-
-getPokemons (); 
-
-/*
     getPokemons().then(pokemons => {
         pokemons.forEach((pokemon, index) => {
           let template = `
